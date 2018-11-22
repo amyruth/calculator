1bc72ee5ea2032ec0a1c23d0b11e1daee10e7fb9
amyruth <anrutherford@gmail.com>
2018-11-22 04:03:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const numButton = document.querySelectorAll('.number-btn');
+const calcWindow = document.querySelector('.calc-window');
+const clearKey = document.querySelector('.clear-btn');
+
+numButton.forEach(button => {
+	button.addEventListener('click', () => {
+		let numValue = button.textContent;
+		calcWindow.value += numValue;
+	});
+});
+
+clearKey.addEventListener('click', () => {
+	calcWindow.value = null;
+});
