50ab4e18e28cd9816156e09aa199f78bfaddb68c
amyruth <anrutherford@gmail.com>
2018-11-25 10:21:40 -0500

    changed number key listener to listen on parent element

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const numButton = document.querySelectorAll(".number-btn");
 const calcWindow = document.querySelector(".calc-window");
 const clearKey = document.querySelector(".clear-btn");
+const keyPad = document.querySelector('.calc-keypad');
+const decimal = document.querySelector('.is-decimal');
 
-numButton.forEach(button => {
-  button.addEventListener("click", () => {
-    let numValue = button.textContent;
-    calcWindow.value += numValue;
-  });
-});
+
+
+// numButton.forEach(button => {
+//   button.addEventListener("click", () => {
+//     let numValue = button.textContent;
+//     calcWindow.value += numValue;
+//   });
+// });
 
 clearKey.addEventListener("click", () => {
   calcWindow.value = null;
+});
+
+decimal.addEventListener('click', () = {
+
+});
+
+// changed to event iistener on parent element. I forgot about that
+keyPad.addEventListener('click', (e) => {
+	console.log(e.target);
+	if(e.target.classList.contains('number-btn')){
+		let numValue = e.target.textContent;
+		calcWindow.value += numValue;
+	}
 });
