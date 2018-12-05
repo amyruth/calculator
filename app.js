efad5d0572af2c6daeb45d7c9f9380df59a4adbd
amyruth <anrutherford@gmail.com>
2018-12-05 00:59:51 -0500

    updated gitignore

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const calcWindow = document.querySelector(".calc-window");
 const clearKey = document.querySelector(".clear-btn");
 const keyPad = document.querySelector('.calc-keypad');
 const decimal = document.querySelector('.is-decimal');
-
+let currentKey = 0;
 
 
 // numButton.forEach(button => {
 //   button.addEventListener("click", () => {
-//     let numValue = button.textContent;
-//     calcWindow.value += numValue;
+//     let currentKey = button.textContent;
+//     calcWindow.value += currentKey;
 //   });
 // });
 
@@ -21,10 +21,28 @@ clearKey.addEventListener("click", () => {
 
 // changed to event iistener on parent element. I forgot about that
 //need to add condition to prevent multiple decimals in a row
+
+// determine if key pressed is decimal
+//is last character entered a number
+
+//if last character is an operator key, add 0 before decimal
+
 keyPad.addEventListener('click', (e) => {
-	// console.log(e.target);
-	if(e.target.classList.contains('number-btn')){
-		let numValue = e.target.textContent;
-		calcWindow.value += numValue;
+	console.log(e.target);
+	
+
+	if(e.target.classList.contains('is-decimal')){
+		//checks if last key pressed was a number
+		//if not a number, do nothing
+		if(!currentKey !== '.'){
+			currentKey += e.target.textContent;
+			console.log(calcWindow.value);
+		}
 	}
+
+	if(e.target.classList.contains('number-btn')){
+		currentKey = e.target.textContent;
+		
+	}
+	calcWindow.value += currentKey;
 });
